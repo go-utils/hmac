17bd6965a7e5e62d356751f108610f4a049e4cbd
54m <30588003+54m@users.noreply.github.com>
2023-07-10 17:56:50 +0900

    add: main code

---
+++ hmac.go
@@ -0,0 +1,35 @@
+package hmac
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+)
+
+//go:generate mockgen -source=$GOFILE -destination=./mock/mock_$GOFILE -package=mock
+
+// Verifier - verifier
+type Verifier interface {
+	Do(message, messageMAC []byte) error
+}
+
+type verifier struct {
+	secretKey []byte
+}
+
+// NewVerifier - constructor
+func NewVerifier(secretKey []byte) Verifier {
+	return &verifier{secretKey: secretKey}
+}
+
+// Do - verify
+func (v *verifier) Do(message, messageMAC []byte) error {
+	mac := hmac.New(sha256.New, v.secretKey)
+	mac.Write(message)
+	expectedMAC := mac.Sum(nil)
+
+	if !hmac.Equal(messageMAC, expectedMAC) {
+		return NotEqual
+	}
+
+	return nil
+}
